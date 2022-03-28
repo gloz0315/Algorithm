b9b129e836b8919f87c826a9b6378ddf6535acc9
Byeong Soo Lee <80665963+gloz0315@users.noreply.github.com>
2022-03-28 17:38:14 +0900

    Create Hash.java

---
+++ 해쉬/Hash.java
@@ -0,0 +1,71 @@
+package Fastcampus;
+
+public class Hash {
+    public Slot[] hashTable;
+
+    public Hash(Integer size) {
+        this.hashTable = new Slot[size];
+    }
+
+    public class Slot {
+        String key;
+        String value;
+        Slot next;
+        Slot(String key, String value) {
+            this.key = key;
+            this.value = value;
+            this.next = null;
+        }
+    }
+
+    public int hashFunc(String key) {
+        return (int)(key.charAt(0)) % this.hashTable.length;
+    }
+
+    public boolean saveData(String key, String value) {
+        Integer address = this.hashFunc(key);
+        if (this.hashTable[address] != null) {
+            Slot findSlot = this.hashTable[address];
+            Slot prevSlot = this.hashTable[address];
+            while (findSlot != null) {
+                if (findSlot.key == key) {
+                    findSlot.value = value;
+                    return true;
+                } else {
+                    prevSlot = findSlot;
+                    findSlot = findSlot.next;
+                }
+            }
+            prevSlot.next = new Slot(key, value);
+        } else {
+            this.hashTable[address] = new Slot(key, value);
+        }
+        return true;
+    }
+
+    public String getData(String key) {
+        Integer address = this.hashFunc(key);
+        if (this.hashTable[address] != null) {
+            Slot findSlot = this.hashTable[address];
+            while (findSlot != null) {
+                if (findSlot.key == key) {
+                    return findSlot.value;
+                } else {
+                    findSlot = findSlot.next;
+                }
+            }
+            return null;
+        } else {
+            return null;
+        }
+    }
+
+    public static void main(String[] args) {
+        Hash hash = new Hash(20);
+        hash.saveData("DaveLee", "01022233333");
+        hash.saveData("DavidLee", "010254653333");
+        hash.saveData("DavesLee", "0102235333");
+        hash.saveData("DaveofLee", "0103897333");
+        hash.getData("DavidLee");
+    }
+}
